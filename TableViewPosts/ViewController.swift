31324892648ae021b4ede3ed6e9192b2fff6ad4c
Daniel Kioko <danielkioko.998@gmail.com>
2021-05-17 19:47:55 -0700

    Initial Commit

---
+++ TableViewPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPosts
+//
+//  Created by Daniel Nzioka on 5/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
